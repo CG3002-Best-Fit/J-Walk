24fc973e98256d964b1b6ee4d67b9896f22e9277
bamboo3250 <bambooVN_3250@yahoo.com>
2015-10-17 10:21:11 +0800

    change IP

--- SocketCommunicator.py
+++ SocketCommunicator.py
@@ -14,7 +14,7 @@ class SocketCommunicator(object):
     isConnectionSuccessful = False
     #RPI_IP = "192.168.1.212"   #Sevin
     #RPI_IP = "192.168.0.109"   #YuShuen
-    RPI_IP = "172.25.100.184"   #COM1
+    RPI_IP = "172.25.103.50"   #COM1
     #COM_IP = "192.168.1.107"   #Sevin
     #COM_IP = "192.168.0.104"   #Yu Shuen
     COM_IP = "172.25.100.199"    #COM1
