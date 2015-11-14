9058217874e606bea801669955fb6298c95218a0
bamboo3250 <bambooVN_3250@yahoo.com>
2015-11-14 09:19:10 +0800

    debug

--- MegaCommunicator.py
+++ MegaCommunicator.py
@@ -64,7 +64,7 @@ class MegaCommunicator(object):
         self.timeout_flag = False
         print "Sending P..."
         rcv = self.send("P")
-        print "rcv from P = " + rcv
+        #print "rcv from P = " + rcv
         
         if self.timeout_flag :
             print "resending P"
